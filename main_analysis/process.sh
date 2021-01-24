6a4f0d75b12f2b0425656e97f458e2df2faa9997
Tyler Ruggles <tylerhruggles@gmail.com>
2021-01-23 22:05:14 -0500

    Cleaned

--- main_analysis/process.sh
+++ main_analysis/process.sh
@@ -1,30 +1,18 @@
 
 
-
-#for REGION in "ERCOT" "NYISO" "PJM"; do
-#for REGION in "ERCOT" "NYISO"; do
-#for REGION in "ERCOT" "NYISO" "PJM"; do
-#for REGION in "ERCOT" "NYISO" "PJM"; do
-#for REGION in "PJM"; do
-#for REGION in "FR"; do
-#for REGION in "PJM"; do
-#for REGION in "ERCOT" "NYISO" "PJM" "FR"; do
-#    python run_main_analysis.py $REGION &
-#done
-
 ############################
 ### Sensitivity analyses ###
 ############################
 ### N Hours
-DATE="20210122v3"
-METHOD="DT"
-YEARS=10
-for REGION in "ERCOT" "NYISO" "PJM" "FR"; do
-    for HOURS in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 25 50 75 100 200; do
-        python run_main_analysis.py ${REGION} ${DATE}${METHOD} ${METHOD} ${HOURS} ${YEARS} "SENSITIVITY" &
-    done
-    sleep 600
-done
+#DATE="20210122v3"
+#METHOD="DT"
+#YEARS=10
+#for REGION in "ERCOT" "NYISO" "PJM" "FR"; do
+#    for HOURS in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 25 50 75 100 200; do
+#        python run_main_analysis.py ${REGION} ${DATE}${METHOD} ${METHOD} ${HOURS} ${YEARS} "SENSITIVITY" &
+#    done
+#    sleep 600
+#done
 
 ### N Years
 #DATE="20210122v2"
@@ -46,18 +34,15 @@ done
 #    done
 #done
 
-DATE="20210120v1"
-#for REGION in "ERCOT"; do # "NYISO" "PJM" "FR"; do
-#    for METHOD in "NOM"; do # "TMY" "PLUS1"; do
-#        #python run_main_analysis.py ${REGION} ${DATE}${METHOD} ${METHOD} 10 &
-#    done
-#done
 
 
 
-#DATE="20210122v1"
-#HOURS=10
-#YEARS=10
-#for REGION in "ERCOT" "NYISO" "PJM" "FR"; do
-#    python plotting.py ${REGION} ${DATE} "DUMMY" ${HOURS} ${YEARS} "Jan22Detrend"
-#done
+############################
+### Plotting             ###
+############################
+DATE="20210122v1"
+HOURS=10
+YEARS=10
+for REGION in "ERCOT" "NYISO" "PJM" "FR"; do
+    python plotting.py ${REGION} ${DATE} "DUMMY" ${HOURS} ${YEARS} "Jan23Detrend"
+done
